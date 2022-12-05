cc437d839bbaa0a44c6bf9117e773231eda9e458
sepwow <dm.galanov1992@gmail.com>
2022-12-05 16:20:36 +0600

    exm_import

--- tests/triangle_tests.py
+++ tests/triangle_tests.py
@@ -2,10 +2,13 @@ import pytest
 from triangle_app import *
 
 
-# p = triangle()
-@pytest.mark.parametrize("a", [3, 1, -1, 0, 250], ids=["positive", "positive", "negative", "zero", "too_much"])
-@pytest.mark.parametrize("b", [4])
-@pytest.mark.parametrize("c", [5])
-def test_triangle(a, b, c):
-    print("a:{0},b:{1},c:{2}".format(a, b, c))
-    assert True
+p = Triangle()
+
+
+@pytest.mark.parametrize("x", [3, 1, -1, 0, 250], ids=["positive", "positive", "negative", "zero", "too_much"])
+@pytest.mark.parametrize("y", [4])
+@pytest.mark.parametrize("z", [5])
+def test_is_triangle(x, y, z):
+    pytest.status, result = p.is_triangle()
+    print("x:{0},y:{1},z:{2}".format(x, y, z))
+    assert pytest.status == True
--- triangle_app.py
+++ triangle_app.py
@@ -1,18 +1,15 @@
-def triangle(a, b, c):
-    flag = ''
-    if a + b <= c:
-        flag = 'c'
-    elif a + c <= b:
-        flag = 'b'
-    elif b + c <= a:
-        flag = 'a'
-    else:
-        return True
+class Triangle:
+    def is_triangle(self, a, b, c):
+        flag = ''
+        if a + b <= c:
+            flag = 'c'
+        elif a + c <= b:
+            flag = 'b'
+        elif b + c <= a:
+            flag = 'a'
+        else:
+            return True
 
-    if flag != '':
-        return False
-        return "'%s' > суммы других" % flag
-
-
-p = triangle(1, 4, 5)
-print(p)
+        if flag != '':
+            return False
+            return "'%s' > суммы других" % flag
